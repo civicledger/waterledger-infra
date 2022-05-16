a24400ba5f9a52e9f641a4e6497e66227c2dfd4a
Matt Burgess <matt@myclient.info>
2022-05-17 01:50:29 +0700

    Update docker compose and supporting documents

--- setup.sh
+++ setup.sh
@@ -3,37 +3,38 @@ RED='\033[0;31m'
 GRAY='\033[1;30m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
 NC='\033[0m'
-applicationName='Water Ledger'
+applicationName="Water Ledger"
 apiRepo=git@github.com:civicledger/wat-adonis-api.git
 dashboardRepo=git@github.com:civicledger/waterledger-dashboard.git
 adminRepo=git@github.com:civicledger/waterledger-admin.git
 
 if [ -d "./api" ]
 then
-    echo "${YELLOW}API repo has already been cloned${NC}"
+    echo -e "${YELLOW}API repo has already been cloned${NC}"
 else
-    echo "Cloning api repo"
+    echo -e "${CYAN}Cloning api repo${NC}"
     git clone $apiRepo api
-    echo "${GREEN}API cloned${NC}"
+    echo -e "${GREEN}API cloned${NC}"
 fi
 
 if [ -d "./dashboard" ]
 then
-    echo "${YELLOW}Dashboard repo has already been cloned${NC}"
+    echo -e "${YELLOW}Dashboard repo has already been cloned${NC}"
 else
-    echo "Cloning dashboard repo"
+    echo -e "${CYAN}Cloning dashboard repo${NC}"
     git clone $dashboardRepo dashboard
-    echo "${GREEN}Dashboard repo cloned${NC}"
+    echo -e "${GREEN}Dashboard repo cloned${NC}"
 fi
 
 if [ -d "./admin" ]
 then
     echo "${YELLOW}Admin repo has already been cloned${NC}"
 else
-    echo "Cloning admin repo"
+    echo -e "${CYAN}Cloning admin repo${NC}"
     git clone $adminRepo admin
     echo -e "${GREEN}Admin repo cloned${NC}"
 fi
 
-echo "${GREEN}${applicationName}${NC} should now be set up. Run ${GRAY}docker-compose up${NC} to launch application."
+echo -e "${GREEN}${applicationName}${NC} should now be set up. Run ${GRAY}docker-compose up${NC} to launch application."
